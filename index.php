111854f35d157b011e4f3dd1a9b9391a42c3c6e9
youlikewhatulook <71880645+youlikewhatulook@users.noreply.github.com>
2020-10-01 08:07:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ require 'db/function.php';
 $db = new Databasedx();
 
 // === QUERY TRACK === //
-
 $allTrack = $db->getAllTrack();
 $getThirdTrack = $db->getThirdTrack();
 
@@ -34,6 +33,8 @@ if( isset($_POST["signup"])) {
   }
 }
 
+
+// CEK LOGIN 
 if( isset($_POST["signin"])) {
   if( !empty($_POST["email"])&&
       !empty($_POST["password"])) {
@@ -57,7 +58,6 @@ if( isset($_POST["signin"])) {
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
